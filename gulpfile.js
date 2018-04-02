20016f3e198680b7b7dc46336685db6c9bb03c30
Roger Forner Fabre <hello@rogerforner.com>
2018-04-02 21:43:23 +0200

    Gulp BrowserSync OK + Jekyll Build :alien:

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
-var gulp     = require('gulp');
-var pump     = require('pump');
-var sass     = require('gulp-sass');
-var uglify   = require('gulp-uglify');
-var concat   = require('gulp-concat');
-var cleanCSS = require('gulp-clean-css');
+var gulp        = require('gulp');
+var pump        = require('pump');
+var sass        = require('gulp-sass');
+var uglify      = require('gulp-uglify');
+var concat      = require('gulp-concat');
+var cleanCSS    = require('gulp-clean-css');
+var shell       = require('gulp-shell');
+var browserSync = require('browser-sync').create();
 
 gulp.task('sass', function(scss) {
   pump([
@@ -24,7 +26,9 @@ gulp.task('stylesheets', function (css) {
     ]),
     cleanCSS({compatibility: 'ie8'}),
     concat('ilercapp.css'),
-    gulp.dest('assets/css')
+    gulp.dest('assets/css'),
+    shell('gulp build'),
+    browserSync.stream()
   ],
     css
   );
@@ -37,14 +41,32 @@ gulp.task('javascript', function (js) {
     ]),
     uglify(),
     concat('ilercapp.js'),
-    gulp.dest('assets/js')
+    gulp.dest('assets/js'),
+    shell('gulp build'),
+    browserSync.stream()
   ],
     js
   );
 });
 
-gulp.task('watch', function(){
-  gulp.watch('src/scss/**/*.scss', ['sass']);
+gulp.task('build', shell.task([
+  'chcp 65001',
+  'jekyll build'
+]));
+
+gulp.task('serve', ['sass', 'stylesheets', 'javascript'], function() {
+  browserSync.init({
+    server: "./_site"
+  });
+
+  gulp.watch("src/scss/**/*.scss", ['sass']);
   gulp.watch('src/css/**/*.css', ['stylesheets']);
   gulp.watch('src/js/**/*.js', ['javascript']);
+  gulp.watch([
+    '_i18n/**/*.html',
+    '_includes/**/*.html',
+    '_layouts/**/*.html',
+    '_posts/**/*.{html, md}'
+  ], ['build', browserSync.reload]);
+  // gulp.watch("_site/**/*.html").on('change', browserSync.reload);
 });
