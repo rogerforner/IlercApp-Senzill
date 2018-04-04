1da3772e1a7758809ec4d4862fd765015f9cae65
Roger Forner Fabre <hello@rogerforner.com>
2018-04-04 12:47:48 +0200

    Gulp refactor

--- gulpfile.js
+++ gulpfile.js
@@ -7,8 +7,13 @@ var uglify       = require('gulp-uglify');
 var concat       = require('gulp-concat');
 var htmlmin      = require('gulp-htmlmin');
 var shell        = require('gulp-shell');
-var browserSync  = require('browser-sync').create();
 
+/*
+# Estils
+*******************************************************************************/
+/*
+## SASS
+----------------------------------------------------------------------------- */
 gulp.task('sass', function(scss) {
   pump([
     gulp.src([
@@ -21,7 +26,10 @@ gulp.task('sass', function(scss) {
   );
 });
 
-gulp.task('stylesheets', function (css) {
+/*
+## CSS
+----------------------------------------------------------------------------- */
+gulp.task('css', function (css) {
   pump([
     gulp.src([
       'src/css/**/*.css'
@@ -32,14 +40,22 @@ gulp.task('stylesheets', function (css) {
     }),
     cleanCSS({compatibility: '*'}),
     concat('ilercapp.css'),
-    gulp.dest('assets/css'),
-    shell('gulp build'),
-    browserSync.stream()
+    gulp.dest('assets/css')
   ],
     css
   );
 });
 
+/*
+## Estils: execució ordenada:
+   1. gulp sass
+   2. gulp css
+----------------------------------------------------------------------------- */
+gulp.task('styles', shell.task('gulp sass && gulp css'));
+
+/*
+# JavaScript
+*******************************************************************************/
 gulp.task('javascript', function (js) {
   pump([
     gulp.src([
@@ -47,14 +63,27 @@ gulp.task('javascript', function (js) {
     ]),
     uglify(),
     concat('ilercapp.js'),
-    gulp.dest('assets/js'),
-    shell('gulp build'),
-    browserSync.stream()
+    gulp.dest('assets/js')
   ],
     js
   );
 });
 
+/*
+# Site
+*******************************************************************************/
+/*
+## Jekyll build
+Si "jekyll build/serve" dona un error de codificació de caràcters, primer s'ha
+d'executar "chcp 65001" i desprès tornar a executar la comanda de jekyll.
+----------------------------------------------------------------------------- */
+gulp.task('build', shell.task([
+  'jekyll build'
+]));
+
+/*
+## Minificar HTML
+----------------------------------------------------------------------------- */
 gulp.task('html', function (html) {
   pump([
     gulp.src('_site/**/*.html', {base: './'}),
@@ -68,25 +97,32 @@ gulp.task('html', function (html) {
   );
 });
 
-gulp.task('build', shell.task([
-  'chcp 65001',
-  'jekyll build',
-  'gulp html'
+/*
+## Site: execució ordenada:
+   1. gulp build
+   2. gulp html
+----------------------------------------------------------------------------- */
+gulp.task('site', shell.task('gulp build && gulp html'));
+
+/*
+# Gulp
+*******************************************************************************/
+gulp.task('default', shell.task([
+  'gulp styles',
+  'gulp javascript',
+  'gulp watch'
 ]));
 
-gulp.task('serve', ['sass', 'stylesheets', 'javascript', 'html'], function() {
-  browserSync.init({
-    server: "./_site"
-  });
-
+gulp.task('watch', ['site'], function() {
   gulp.watch('src/scss/**/*.scss', ['sass']);
-  gulp.watch('src/css/**/*.css', ['stylesheets']);
+  gulp.watch('src/css/**/*.css', ['css']);
   gulp.watch('src/js/**/*.js', ['javascript']);
   gulp.watch([
     '_i18n/**/*.html',
     '_includes/**/*.html',
     '_layouts/**/*.html',
-    '_posts/**/*.{html, md}'
-  ], ['build', browserSync.reload]);
-  gulp.watch("_site/**/*.html", ['html', browserSync.reload]);
+    '_posts/**/*.{html, md}',
+    './*.html',
+  ], ['site']);
+  // gulp.watch("_site/**/*.html", ['html']);
 });
